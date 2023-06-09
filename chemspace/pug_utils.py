333ac5291a893e44c47e5f975d08bb992cdb5a40
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2023-06-09 16:49:18 -0400

    decrease lowest default wait time, return int

--- chemspace/pug_utils.py
+++ chemspace/pug_utils.py
@@ -108,8 +108,9 @@ def download_compounds(start_cid, end_cid):
     return [name_response, desc_response], names, smiless, descriptions
 
 
-def regulate_api_requests(response: str) -> float:
-    wait_time = 0.2
+def regulate_api_requests(response: str) -> int:
+    # Default wait time that has been in use
+    wait_time = 0
     
     parse_throttling_headers(response.headers['X-Throttling-Control'])
 
