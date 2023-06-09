98b2ad9337bffdd5f131b64ad2fc26520b12a691
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2023-06-09 15:26:37 -0400

    Add basic github workflow (#5)
    * removed unneeded imports and fixed requirements files
    
    * add basic workflow
    
    only ensures dependencies can be installed for now
    
    * change unit test file name
    
    * change workflow name
    
    * change name of env `.yml`
    
    * add dummy unit test
    
    * uncomment pytest job
    
    * update concurrency settings
    
    * add pytest to dependencies
    
    ---------
    
    Co-authored-by: maykcaldas <maykcaldas@gmail.com>

---
+++ tests/test_pug_utils.py
@@ -0,0 +1,7 @@
+class TestPUGUtils:
+    def test_dummy_test(self):
+        """
+        Placeholder test to initialize test suite
+        Can be deleted when we have actual tests
+        """
+        assert True
