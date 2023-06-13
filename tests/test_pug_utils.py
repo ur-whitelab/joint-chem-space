60d3c50c30ca8f055978fadb6d7e33c6141e55cf
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2023-06-13 09:22:31 -0400

    Moderate REST API requests (#8)
    * add `ipykernel` `datasets` and `seaborn` to reqs
    
    * return api responses
    
    * include left out return statement
    
    * fix typo
    
    * WIP add function to regulate api request wait time
    
    * Update environment.yml
    
    * add function to parse the X-throttling header
    
    * include imports
    
    * calculate appropriate wait before each api request
    
    * return df of statuses instead of dict
    
    * cast status text to lowercase
    
    * decrease lowest default wait time, return int
    
    * Revert "decrease lowest default wait time, return int"
    
    This reverts commit 333ac5291a893e44c47e5f975d08bb992cdb5a40.
    
    * add placeholder logic for adjusting request interval
    
    * add wait times for non-green statues
    
    * make returned times type `float`
    
    * change default spec
    
    * add docstring
    
    * add comments
    
    * lower wait times for red and yellow statues
    
    * add tests for api regulation
    
    * don't return responses from `download_compounds`
    
    * remove dummy test
    
    * add IDs to parameterized test
    
    * redo blocked requests

--- tests/test_pug_utils.py
+++ tests/test_pug_utils.py
@@ -1,7 +1,33 @@
+import sys
+sys.path.append(".")
+
+import pytest
+from requests.models import Response
+
+from chemspace.pug_utils import regulate_api_requests
+
 class TestPUGUtils:
-    def test_dummy_test(self):
-        """
-        Placeholder test to initialize test suite
-        Can be deleted when we have actual tests
-        """
-        assert True
+    @pytest.mark.parametrize("headers,wait_time",[
+        ("Request Count status: Green (0%), Request Time status: Green (0%), Service status: Green (20%)", 0.2),
+        ("Request Count status: Green (0%), Request Time status: Green (0%), Service status: Black (99%)", 3600.0),
+        ("Request Count status: Green (0%), Request Time status: Red (80%), Service status: Green (20%)", 60.0),
+        ("Request Count status: Yellow (40%), Request Time status: Green (0%), Service status: Green (20%)", 1.0),
+        ],
+        ids = [
+            "Green",
+            "Black",
+            "Red",
+            "Yellow",
+        ]
+        )
+    def test_rest_regulation(self, headers, wait_time):
+        # Build mock response
+        response = Response()
+        response.status_code = 200
+        response.headers = {'X-Throttling-Control': headers}
+        
+        # Get appropriate wait time from funciton
+        test_wait_time = regulate_api_requests(response)
+
+        # Ensure it matches what is expected
+        assert test_wait_time == wait_time
