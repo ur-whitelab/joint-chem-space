468bacb5254387e32e7481301b9b49648f13a099
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2023-06-13 16:08:14 -0400

    allow a dataframe to be passed in instead of list

--- chemspace/Dataset/DatasetBuilder.py
+++ chemspace/Dataset/DatasetBuilder.py
@@ -4,15 +4,16 @@ import json
 import pandas as pd
 from json import JSONDecodeError
 
-from typing import List
-
 class DatasetBuilder:
     def __init__(
         self,
         compound_list_path: str = None,
-        compound_list: List = None,
+        compound_df: pd.DataFrame = None,
         ):
+        # Initialize list to hold CIDs
         self.CIDs = []
+
+        # If path passed in, open and parse lines to get CIDs
         if compound_list_path:
             with gzip.open(compound_list_path, 'rt') as zipfile:
                 for line in zipfile:
@@ -21,7 +22,10 @@ class DatasetBuilder:
                         dict = json.loads(data[0:-2])                        
                         self.CIDs.append(dict['id']['id']['cid'])
                     else:
-                        break
-
-        self.dataset = pd.DataFrame(index = self.CIDs)
-        return
+                        self.dataset = pd.DataFrame(index = self.CIDs)
+                        return
+        # If dataframe passed in, assign to self.dataset
+        elif compound_df:
+            self.dataset = compound_df
+            return
+        
