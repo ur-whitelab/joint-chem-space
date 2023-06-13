b3b01159808ab531f6bdf468624307506441ce5f
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2023-06-13 16:09:38 -0400

    allow CSV path to be passed in as well

--- chemspace/Dataset/DatasetBuilder.py
+++ chemspace/Dataset/DatasetBuilder.py
@@ -7,23 +7,29 @@ from json import JSONDecodeError
 class DatasetBuilder:
     def __init__(
         self,
-        compound_list_path: str = None,
+        compound_file_path: str = None,
         compound_df: pd.DataFrame = None,
         ):
         # Initialize list to hold CIDs
         self.CIDs = []
 
-        # If path passed in, open and parse lines to get CIDs
-        if compound_list_path:
-            with gzip.open(compound_list_path, 'rt') as zipfile:
-                for line in zipfile:
-                    data = zipfile.readline()
-                    if data and len(data) > 3:
-                        dict = json.loads(data[0:-2])                        
-                        self.CIDs.append(dict['id']['id']['cid'])
-                    else:
-                        self.dataset = pd.DataFrame(index = self.CIDs)
-                        return
+        # If path passed in, open as appropriate and get CIDs
+        if compound_file_path:
+            # If file is the compressed json file from PubChem, parse and save
+            if compound_file_path.endswith('.json.gz'):
+                with gzip.open(compound_file_path, 'rt') as zipfile:
+                    for line in zipfile:
+                        data = zipfile.readline()
+                        if data and len(data) > 3:
+                            dict = json.loads(data[0:-2])                        
+                            self.CIDs.append(dict['id']['id']['cid'])
+                        else:
+                            self.dataset = pd.DataFrame(index = self.CIDs)
+                            return
+            # IF passing in a csv, open as appropriate
+            elif compound_file_path.endswith('.csv'):
+                self.dataset = pd.read_csv(compound_file_path, index_col='Unnamed: 0')
+                return
         # If dataframe passed in, assign to self.dataset
         elif compound_df:
             self.dataset = compound_df
