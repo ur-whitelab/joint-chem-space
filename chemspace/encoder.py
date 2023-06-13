8fc45c1a430a694901155849a6310e017c5b668e
DataProdeng <96993587+ahmadovra@users.noreply.github.com>
2023-06-13 11:12:05 -0400

    smiles encoder added

--- chemspace/encoder.py
+++ chemspace/encoder.py
@@ -4,7 +4,6 @@ import torch
 from torch.utils.data import Dataset
 from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification, TrainerCallback, TrainingArguments, Trainer
 
-
 #create data handlers
 #torchy way of creating dataclass
 class data_loader:
