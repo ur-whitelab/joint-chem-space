c6d0273806154ce5fa3611c41e2f3d17cf715df5
maykcaldas <maykcaldas@gmail.com>
2023-06-14 09:37:48 -0400

    Revert "Implemented train with the smiles and textual branches"
    This reverts commit 2f3eb45d161de9669ebdd941676d1ec962cb9d70.

--- experiments/train.py
+++ experiments/train.py
@@ -1,13 +1,12 @@
 import sys
 sys.path.append("..")
 
-from chemspace import ProjConfig, Projector, Encoder, loss_function
+from chemspace import ProjConfig, Projector, loss_function
 
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 
-import pandas as pd
 
 def rmse(zi, zj):
   return torch.sqrt(torch.mean((zi-zj)**2))
@@ -17,7 +16,7 @@ def rmse(zi, zj):
 class DummyDataset(Dataset):
     def __init__(self,
                  num_samples: int = 100
-                ):
+                 ):
         self.num_samples = num_samples
         self.data = list(zip(
             torch.randn(num_samples, 32),
@@ -30,81 +29,52 @@ class DummyDataset(Dataset):
     def __len__(self):
         return self.num_samples
 
-class PubChemDataset(Dataset):
-    def __init__(self, 
-                 path: str
-                ) -> None:
-      self.data = pd.read_csv(path)
-
-    def __getitem__(self, index) -> tuple[str, str]:
-      return (self.data["SMILES"][index], self.data["Description"][index])
-    
-    def __len__(self) -> int:
-      return len(self.data)
-
 def train(dataset: Dataset,
-          num_epochs: int = 5,
-          batch_size: int = 32,
+          num_epochs: int = 50,
+          batch_size: int = 16,
           optimizer: optim = None,
-          E1: Encoder = None,
           P1: Projector = None,
-          E2: Encoder = None,
-          P2: Projector = None, # Add encoders/projectors as a list? Addapt the loss to receive n encoders/projectors?
-          ) -> None:
-  for i in range(1, num_epochs+1):
+          P2: Projector = None, # Add projectors as a list? Addapt the loss to receive n projectors?
+          ):
+  for i in range(num_epochs):
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     for batch in dataloader:
       
-      x_sml, x_desc = batch[0], batch[1]
-      z_sml = P1(E1(x_sml).to(device))
-      z_desc = P2(E2(x_desc).to(device))
+      x1, x2 = batch[0].to(device), batch[1].to(device)
+      z1 = P1(x1)
+      z2 = P2(x2)
 
-      loss = loss_function(z_sml, z_desc)
+      loss = loss_function(z1, z2)
 
       optimizer.zero_grad()
       loss.backward()
       optimizer.step()
     
-    if not i%1:
+    if not i%10:
       print (f'Epoch: {i} == Loss: {loss.item():.4f}')
 
 if __name__ == "__main__":
   # Create two dummy projectores
-  P1config = ProjConfig(input_size=384)
+  P1config = ProjConfig(input_size=32)
   P1 = Projector(**vars(P1config))
-  E1 = Encoder()
 
-  P2config = ProjConfig(input_size=384)
+  P2config = ProjConfig(input_size=16)
   P2 = Projector(**vars(P2config))
-  E2 = Encoder()
 
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-  # dataset = DummyDataset()
-  dataset = PubChemDataset(path="../Data/PubChem500.csv")
-
-  #Quick test
-  # Same entity
-  k = 0
-  z1 = P1(E1(dataset[k][0]))
-  z2 = P2(E2(dataset[k][1]))
-  print(f" RMSE for the same entry: {rmse(z1,z2)}")
-  # different entity
-  z1 = P1(E1(dataset[k][0]))
-  z2 = P2(E2(dataset[k+88][1]))
-  print(f" RMSE for different entries: {rmse(z1,z2)}")
-
   # Start training
+  dataset = DummyDataset()
   optimizer = optim.Adam(list(P1.parameters()) + list(P2.parameters()), lr=0.001)
-  train(dataset, optimizer=optimizer, E1=E1, P1=P1, E2=E2, P2=P2)
+  train(dataset, optimizer=optimizer, P1=P1, P2=P2)
 
 # Same entity
 k = 0
-z1 = P1(E1(dataset[k][0]))
-z2 = P2(E2(dataset[k][1]))
+z1 = P1(dataset[k][0])
+z2 = P2(dataset[k][1])
 print(f" RMSE for the same entry: {rmse(z1,z2)}")
 
 # different entity
-z1 = P1(E1(dataset[k][0]))
-z2 = P2(E2(dataset[k+88][1]))
+z1 = P1(dataset[k][0])
+z2 = P2(dataset[k+88][1])
 print(f" RMSE for different entries: {rmse(z1,z2)}")
