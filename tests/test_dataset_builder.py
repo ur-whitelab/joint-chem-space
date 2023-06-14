ec973b3ac8dd2b8eed5b1ed7a4cde184abd3ed83
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2023-06-14 11:17:09 -0400

    use fixtures for file paths

--- tests/test_dataset_builder.py
+++ tests/test_dataset_builder.py
@@ -8,14 +8,24 @@ import pytest
 import chemspace as cs
 from chemspace.Dataset.DatasetBuilder import DatasetBuilder
 
+
+@pytest.fixture
+def pubchem_compund_report_path():
+    return './chemspace/Dataset/Data/PubChem_compound_list_records.json.gz'
+
+@pytest.fixture
+def dataset_CSV_path():
+    return './chemspace/Dataset/Data/CompoundDataset.csv'
+
 class TestDatasetBuilder:
     
-    @pytest.mark.parametrize('compound_file_path',['./chemspace/Dataset/Data/PubChem_compound_list_records.json.gz','./chemspace/Dataset/Data/CompoundDataset.csv'])
-    def test_instantiate_DB_from_file(self, compound_file_path):
+    @pytest.mark.parametrize('compound_file_path',['pubchem_compund_report_path', 'dataset_CSV_path'])
+    def test_instantiate_DB_from_file(self, compound_file_path, request):
         """
         Tests to cover instantiating a DatasetBuilder object from a zipped JSON file or a CSV
         """
         # Get the absolute path to the file and create DatasetBuilder instance
+        compound_file_path = request.getfixturevalue(compound_file_path)
         compound_file_path = os.path.abspath(compound_file_path)
         DB = DatasetBuilder(compound_file_path=compound_file_path)
 
