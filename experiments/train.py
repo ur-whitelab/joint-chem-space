78404e4086725815e5c0f57bd8dee1771a2ed31b
maykcaldas <maykcaldas@gmail.com>
2023-06-14 13:19:19 -0400

    Updated variable names to be more meaningful

--- experiments/train.py
+++ experiments/train.py
@@ -46,18 +46,18 @@ def train(dataset: Dataset,
           num_epochs: int = 5,
           batch_size: int = 32,
           optimizer: optim = None,
-          E1: Encoder = None,
-          P1: Projector = None,
-          E2: Encoder = None,
-          P2: Projector = None, # Add encoders/projectors as a list? Addapt the loss to receive n encoders/projectors?
+          E_sml: Encoder = None,
+          P_sml: Projector = None,
+          E_desc: Encoder = None,
+          P_desc: Projector = None, # Add encoders/projectors as a list? Addapt the loss to receive n encoders/projectors?
           ) -> None:
   for i in range(1, num_epochs+1):
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     for batch in dataloader:
       
       x_sml, x_desc = batch[0], batch[1]
-      z_sml = P1(E1(x_sml).to(device))
-      z_desc = P2(E2(x_desc).to(device))
+      z_sml = P_sml(E_sml(x_sml).to(device))
+      z_desc = P_desc(E_desc(x_desc).to(device))
 
       loss = loss_function(z_sml, z_desc)
 
@@ -70,41 +70,41 @@ def train(dataset: Dataset,
 
 if __name__ == "__main__":
   # Create two dummy projectores
-  P1config = ProjConfig(input_size=384)
-  P1 = Projector(**vars(P1config))
-  E1 = Encoder()
+  P_sml_config = ProjConfig(input_size=384)
+  P_sml = Projector(**vars(P_sml_config))
+  E_sml = Encoder()
 
-  P2config = ProjConfig(input_size=384)
-  P2 = Projector(**vars(P2config))
-  E2 = Encoder()
+  P_desc_config = ProjConfig(input_size=384)
+  P_desc = Projector(**vars(P_desc_config))
+  E_desc = Encoder()
 
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
   # dataset = DummyDataset()
-  dataset = PubChemDataset(path="../Data/PubChem500.csv")
+  dataset = PubChemDataset(path="../chemspace/Dataset/Data/PubChem.csv")
 
   #Quick test
   # Same entity
   k = 0
-  z1 = P1(E1(dataset[k][0]))
-  z2 = P2(E2(dataset[k][1]))
+  z1 = P_sml(E_sml(dataset[k][0]))
+  z2 = P_desc(E_desc(dataset[k][1]))
   print(f" RMSE for the same entry: {rmse(z1,z2)}")
   # different entity
-  z1 = P1(E1(dataset[k][0]))
-  z2 = P2(E2(dataset[k+88][1]))
+  z1 = P_sml(E_sml(dataset[k][0]))
+  z2 = P_desc(E_desc(dataset[k+88][1]))
   print(f" RMSE for different entries: {rmse(z1,z2)}")
 
   # Start training
-  optimizer = optim.Adam(list(P1.parameters()) + list(P2.parameters()), lr=0.001)
-  train(dataset, optimizer=optimizer, E1=E1, P1=P1, E2=E2, P2=P2)
+  optimizer = optim.Adam(list(P_sml.parameters()) + list(P_desc.parameters()), lr=0.001)
+  train(dataset, optimizer=optimizer, E_sml=E_sml, P_sml=P_sml, E_desc=E_desc, P_desc=P_desc)
 
 # Same entity
 k = 0
-z1 = P1(E1(dataset[k][0]))
-z2 = P2(E2(dataset[k][1]))
+z1 = P_sml(E_sml(dataset[k][0]))
+z2 = P_desc(E_desc(dataset[k][1]))
 print(f" RMSE for the same entry: {rmse(z1,z2)}")
 
 # different entity
-z1 = P1(E1(dataset[k][0]))
-z2 = P2(E2(dataset[k+88][1]))
+z1 = P_sml(E_sml(dataset[k][0]))
+z2 = P_desc(E_desc(dataset[k+88][1]))
 print(f" RMSE for different entries: {rmse(z1,z2)}")
