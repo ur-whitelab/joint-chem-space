29e8fa0540a2a1230f940ba12d0e2a48848ce189
maykcaldas <maykcaldas@gmail.com>
2023-06-15 14:03:04 -0400

    Added normalization layers

--- chemspace/Projectors.py
+++ chemspace/Projectors.py
@@ -5,9 +5,10 @@ from dataclasses import dataclass
 
 @dataclass
 class ProjConfig:
-  input_size  : int         = 32
-  hidden_size : int         = 16
-  output_size : int         = 32
+  input_size  : int         = 384
+  hidden_size : int         = 256
+  output_size : int         = 256
+  dropout_rate : float      = 0.2
   activation_function : str = "relu"
 
 
@@ -16,6 +17,7 @@ class Projector(nn.Module):
                  input_size: int,
                  hidden_size: int,
                  output_size: int,
+                 dropout: float = 0.2,
                  *args,
                  **kwargs
                  ):
@@ -23,27 +25,33 @@ class Projector(nn.Module):
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, output_size)
-        self.relu = nn.GELU() # do we want to expose this?
+        self.relu = nn.ReLU() 
+        self.tanh = nn.Tanh() 
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm1 = nn.LayerNorm(hidden_size)
+        self.layer_norm2 = nn.LayerNorm(hidden_size)
+        self.layer_norm3 = nn.LayerNorm(output_size)
+        # self.init_weights()
 
     def forward(self, x):
-        out = self.relu(self.fc1(x))
-        out = self.relu(self.fc2(out))
-        out = self.relu(self.fc3(out))
-        return out
+        out = self.fc1(x)
+        out = self.dropout(self.relu(self.layer_norm1(out)))
+        out = self.fc2(out)
+        out = self.dropout(self.relu(self.layer_norm2(out)))
+        out = self.fc3(out)
+        out = self.dropout(self.relu(self.layer_norm3(out)))
+        return self.tanh(out)
+      
+    def init_weights(self):
+        initrange = 0.1
+        self.fc1.weight.data.uniform_(-initrange, initrange)
+        self.fc1.bias.data.zero_()
+        self.fc2.weight.data.uniform_(-initrange, initrange)
+        self.fc2.bias.data.zero_()
+        self.fc3.weight.data.uniform_(-initrange, initrange)
+        self.fc3.bias.data.zero_()
 
-
-def cos_sim(v1, v2):
-  return torch.nn.functional.cosine_similarity(v1, v2)
-
-
-def loss_function(zi, zj):
-  def sigma(E):
-    '''
-    Part of the EBM-NCE loss function
-    https://openreview.net/pdf?id=xQUe1pOKPam: page 23
-    '''
-    return 1 / (torch.exp(-E)+1)
-
-  noise = torch.randn(32)
-  return -0.5 * ( torch.log(sigma(cos_sim(zi,zj))).mean() + torch.log(sigma(1-cos_sim(zi, noise))).mean() ) + \
-         -0.5 * ( torch.log(sigma(cos_sim(zi,zj))).mean() + torch.log(sigma(1-cos_sim(noise, zj))).mean() )
+if __name__ == "__main__":
+    m = Projector(**vars(ProjConfig()))
+    test = torch.randn(1,384)
+    print(m(test).shape)
