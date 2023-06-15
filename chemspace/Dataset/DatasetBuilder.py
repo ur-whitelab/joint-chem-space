bfdf20b8dd3d39396e2c9653804b2cdb0695118f
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2023-06-15 14:54:30 -0400

    WIP add SMILES to CID df

--- chemspace/Dataset/DatasetBuilder.py
+++ chemspace/Dataset/DatasetBuilder.py
@@ -47,9 +47,30 @@ class DatasetBuilder:
         
     def add_SMILES(self, data_path: str = '../chemspace/Dataset/Data/CID-SMILES.gz'):
         data_reader = pd.read_csv(data_path, chunksize= 10 ** 6, sep= "\t", names = ['CID', 'SMILES'], index_col = 'CID')
-
+        i = 0
         for df in data_reader:
-            print(df)
+            if i % 5 == 0:
+                print(i)
 
+            #print(df)
+            if df.index[0] > self.CIDs.index[-1]:
+                return
+            elif self._external_CIDs_in_dataset(df.index):
+                try:
+                    self.dataset = pd.concat([self.dataset, self.CIDs.merge(df, how='left', left_index = True, right_index=True, suffixes= (None,'_y'))], axis = 0)
+                except(pd.errors.MergeError):
+                    print(df)
+            i = i + 1
         return
     
+    def _external_CIDs_in_dataset(self, external_CIDs: pd.Index) -> bool:
+        """
+        Function that compares indices of the Dataset to a set of indices passed in.
+        Args:
+            external_CIDs: CID indices of an external dataframe
+        Returns:
+            True: If any of the dataset CIDs are present in the external dataframe
+            False: If none of the dataset CIDS are present in the external dataframe
+
+        """
+        return self.CIDs.index.isin(external_CIDs).any()
