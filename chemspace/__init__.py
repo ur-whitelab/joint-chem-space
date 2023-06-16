822bee1d2ead16504b629439c0d1d886f977c5ca
maykcaldas <maykcaldas@gmail.com>
2023-06-16 14:48:50 -0400

    removed import of loss function from Projectors

--- chemspace/__init__.py
+++ chemspace/__init__.py
@@ -1,4 +1,4 @@
 from .pug_utils import *
 from .obabel_utils import *
 from .Encoders import Encoder
-from .Projectors import ProjConfig, Projector, loss_function
+from .Projectors import ProjConfig, Projector
