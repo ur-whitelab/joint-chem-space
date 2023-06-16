293d122ddf99404bfc2e4c66691f46291d3ab57d
maykcaldas <maykcaldas@gmail.com>
2023-06-16 09:42:33 -0400

    removed mace from encoder branch
    This feature was moved to the mace-encoder branch while we study how to implement it

--- mace-encoder/mace/calculators/__init__.py
+++
@@ -1,9 +0,0 @@
-from .lammps_mace import LAMMPS_MACE
-from .mace import DipoleMACECalculator, EnergyDipoleMACECalculator, MACECalculator
-
-__all__ = [
-    "MACECalculator",
-    "DipoleMACECalculator",
-    "EnergyDipoleMACECalculator",
-    "LAMMPS_MACE",
-]
