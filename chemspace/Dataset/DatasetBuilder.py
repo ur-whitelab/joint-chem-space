cce750e0ae92454750f398f44c66c2a942a0c420
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2023-06-21 09:06:41 -0400

    Add SMILES to dataset (#20)
    * fix off by 2 error with importing CIDs
    
    * complete CID list
    
    * update notebook
    
    * update tests and add checks
    
    * WIP add basic function to add SMILES data to dataset
    
    * spacing
    
    * start using notebook for new method
    
    * WIP add SMILES to CID df
    
    * functionality to load SMILES works as intended
    
    * clean and comments
    
    * notebook updates for SMILES development
    
    * first iteration of the dataset: CIDs and SMILES
    
    * save smiles df as dataset
    
    * save CIDs as a pandas series instead of index
    
    * create empty df to use. Store CIDs as column
    
    * update DF col name
    
    * change col name to CID
    
    * fix switch from index to series
    
    * update tests for new structure
    
    * update structure when init using df
    
    * update notebook

--- chemspace/Dataset/DatasetBuilder.py
+++ chemspace/Dataset/DatasetBuilder.py
@@ -31,17 +31,56 @@ class DatasetBuilder:
                         except(json.JSONDecodeError):
                             continue
                         
-                    self.CIDs = pd.DataFrame(index = CIDs)
+                    self.CIDs = pd.Series(CIDs, name='CID', dtype = 'int64')
                     self.dataset = pd.DataFrame()
                     return
             # IF passing in a csv, open as appropriate
             elif compound_file_path.endswith('.csv'):
-                self.CIDs = pd.read_csv(compound_file_path, index_col='Unnamed: 0')
-                self.dataset = pd.DataFrame()
+                self.dataset = pd.read_csv(compound_file_path,)
+                self.CIDs = self.dataset['CID']
                 return
         # If dataframe passed in, assign to self.dataset
         elif compound_df is not None:
             self.dataset = compound_df
-            self.CIDs = compound_df.index
+            self.CIDs = compound_df['CID']
             return
         
+    def add_SMILES(self, data_path: str = '../chemspace/Dataset/Data/CID-SMILES.gz'):
+        concat_df = pd.DataFrame()
+        
+        # create object to iterate through CSV chunks
+        data_reader = pd.read_csv(data_path, chunksize= 10 ** 6, sep= "\t", names = ['CID', 'SMILES'])
+        
+        # Initialize counter and 
+        i = 0
+
+        # Iterate through df chunks
+        for df in data_reader:
+            # Display progress
+            if i % 5 == 0:
+                print(i)
+
+            # if the lowest CID of the chunk is greater than the largest CID we're interested in, then stop
+            if df['CID'].iloc[0] > self.CIDs.iloc[-1]:
+                return
+            # Otherwise, merge the CIDs and SMILES chunk dataframes, and concat. Store as dataset
+            elif self._external_CIDs_in_dataset(df['CID']):
+                merged_df = self.CIDs.to_frame().merge(df, how='inner', left_on = 'CID', right_on='CID', suffixes= (None,'_y'))
+                concat_df = pd.concat([concat_df, merged_df], axis = 0, ignore_index=True)
+            
+            # Advance counter
+            i = i + 1
+            self.dataset = concat_df
+        return
+    
+    def _external_CIDs_in_dataset(self, external_CIDs: pd.Index) -> bool:
+        """
+        Function that compares indices of the Dataset to a set of indices passed in.
+        Args:
+            external_CIDs: CID indices of an external dataframe
+        Returns:
+            True: If any of the dataset CIDs are present in the external dataframe
+            False: If none of the dataset CIDS are present in the external dataframe
+
+        """
+        return self.CIDs.isin(external_CIDs).any()
