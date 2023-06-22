2df91c48dc9f24216725872210109ae36d4813ad
maykcaldas <maykcaldas@gmail.com>
2023-06-22 15:41:23 -0400

    preparing to merge into main

--- experiments/train.py
+++ experiments/train.py
@@ -79,13 +79,15 @@ class PubChemDataset(Dataset):
                  frac: Optional[Union[float, None]] = None
                 ) -> None:
       
+        print(f"Loading dataset from {path}")
         self.data = pd.read_csv(path)
+        self.data = self.data[['SMILES', 'AllText']].dropna().reset_index(drop=True)
         if frac:
             self.data = self.data.sample(frac=frac).reset_index(drop=True)
 
     def __getitem__(self, index):
         sml = self.data["SMILES"][index]
-        desc = self.data["Description"][index]
+        desc = self.data["AllText"][index]
         return {
             "sml": sml,
             "desc": desc
@@ -93,62 +95,6 @@ class PubChemDataset(Dataset):
     
     def __len__(self) -> int:
         return len(self.data)
-
-
-def train(dataset: Dataset,
-          num_epochs: int = 50,
-          batch_size: int = 32,
-          optimizer: optim = None,
-          E_sml: Encoder = None,
-          P_sml: Projector = None,
-          E_desc: Encoder = None,
-          P_desc: Projector = None, # Add encoders/projectors as a list? Addapt the loss to receive n encoders/projectors?
-          ) -> None:
-  for i in range(1, num_epochs+1):
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-    E_sml.model.eval()
-    E_desc.model.eval()
-
-    for param in E_sml.model.parameters():
-      param.requires_grad = False
-    for param in E_desc.model.parameters():
-      param.requires_grad = False
-
-    P_sml.train()
-    P_desc.train()
-
-    same_entry_distances = []
-    different_entry_distances = []
-    losses = []
-    for batch in dataloader:
-        optimizer.zero_grad()
-
-        x_sml, x_desc = batch['sml'], batch['desc']
-        z_sml = P_sml(E_sml(x_sml).to(device))
-        z_desc = P_desc(E_desc(x_desc).to(device))
-        shift = torch.randint(low=1, high=batch_size, size=(1,)).item()
-
-        loss = loss_fxn(z_sml, z_desc, z_desc.roll(shift, 0))
-        loss.backward()
-        optimizer.step()
-
-        losses.append(loss.item())
-        same_entry_distance = distance(z_sml, z_desc)
-        different_entry_distance = distance(z_sml, z_desc.roll(shift, 0))
-        same_entry_distances.append(same_entry_distance.mean().item())
-        different_entry_distances.append(different_entry_distance.mean().item())
-
-    # Calculate average distances
-
-    avg_loss = sum(losses) / len(losses)
-    avg_same_entry_distance = sum(same_entry_distances) / len(same_entry_distances)
-    avg_different_entry_distance = sum(different_entry_distances) / len(different_entry_distances)
-
-    if not i%1:
-        print (f'Epoch: {i}\n\tLoss: {avg_loss}')
-        print(f"\tAverage distance for the same entries: {avg_same_entry_distance}")
-        print(f"\tAverage distance for different entries: {avg_different_entry_distance}")
 
 
 def test(dataset: Dataset,
@@ -188,6 +134,63 @@ def test(dataset: Dataset,
     print(f"\tAverage distance for the same entries: {avg_same_entry_distance}")
     print(f"\tAverage distance for different entries: {avg_different_entry_distance}")
 
+
+def train(dataset: Dataset,
+          num_epochs: int = 50,
+          batch_size: int = 32,
+          optimizer: optim = None,
+          E_sml: Encoder = None,
+          P_sml: Projector = None,
+          E_desc: Encoder = None,
+          P_desc: Projector = None, # Add encoders/projectors as a list? Addapt the loss to receive n encoders/projectors?
+          ) -> None:
+  E_sml.model.eval()
+  E_desc.model.eval()
+
+  for param in E_sml.model.parameters():
+    param.requires_grad = False
+  for param in E_desc.model.parameters():
+    param.requires_grad = False
+
+  P_sml.train()
+  P_desc.train()
+  for i in range(1, num_epochs+1):
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    same_entry_distances = []
+    different_entry_distances = []
+    losses = []
+    for batch in dataloader:
+        optimizer.zero_grad()
+
+        x_sml, x_desc = batch['sml'], batch['desc']
+        z_sml = P_sml(E_sml(x_sml).to(device))
+        z_desc = P_desc(E_desc(x_desc).to(device))
+        shift = torch.randint(low=1, high=batch_size, size=(1,)).item()
+
+        loss = loss_fxn(z_sml, z_desc, z_desc.roll(shift, 0))
+        loss.backward()
+        optimizer.step()
+
+        losses.append(loss.item())
+        same_entry_distance = distance(z_sml, z_desc)
+        different_entry_distance = distance(z_sml, z_desc.roll(shift, 0))
+        same_entry_distances.append(same_entry_distance.mean().item())
+        different_entry_distances.append(different_entry_distance.mean().item())
+
+    # Calculate average distances
+
+    avg_loss = sum(losses) / len(losses)
+    avg_same_entry_distance = sum(same_entry_distances) / len(same_entry_distances)
+    avg_different_entry_distance = sum(different_entry_distances) / len(different_entry_distances)
+
+    if not i%1:
+        print (f'Epoch: {i}\n\tLoss: {avg_loss}')
+        print(f"\tAverage distance for the same entries: {avg_same_entry_distance}")
+        print(f"\tAverage distance for different entries: {avg_different_entry_distance}")
+
+
+
 if __name__ == "__main__":
     P_sml_config = ProjConfig(input_size=384)
     P_sml = Projector(**vars(P_sml_config))
@@ -198,18 +201,28 @@ if __name__ == "__main__":
     E_desc = Encoder()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
 
-    dataset = PubChemDataset(path="../chemspace/Dataset/Data/PubChem.csv")
+    # dataset = PubChemDataset(path="../chemspace/Dataset/Data/PubChem.csv")
+    dataset = PubChemDataset(path="../chemspace/Dataset/Data/Dataset.gz")
+    
     split = [int(len(dataset)*0.8)+2, int(len(dataset)*0.1), int(len(dataset)*0.1)]
     print(len(dataset), sum(split))
     train_data, test_data, val_data = torch.utils.data.random_split(dataset, split)
     print(len(train_data), len(test_data), len(val_data))
 
-    # Start training
-    optimizer = optim.Adam(list(P_sml.parameters()) + list(P_desc.parameters()), lr=0.005)
+    optimizer = optim.Adam(list(P_sml.parameters()) + list(P_desc.parameters()), lr=0.05)
     train(train_data, optimizer=optimizer, E_sml=E_sml, P_sml=P_sml, E_desc=E_desc, P_desc=P_desc)
 
-    # Start testing
     test(test_data, E_sml=E_sml, P_sml=P_sml, E_desc=E_desc, P_desc=P_desc)
+
+    torch.save(P_sml.state_dict(), "P_sml.pt")
+    torch.save(P_desc.state_dict(), "P_desc.pt")
+
+    # Saving for later
+    # P_sml.load_state_dict(torch.load("P_sml.pt"))
+    # P_desc.load_state_dict(torch.load("P_desc.pt"))
+
+
 
 
