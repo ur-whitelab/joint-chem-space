ca8674a7c3518a7f4fb4d1ccfa721553a44d43f3
maykcaldas <maykcaldas@gmail.com>
2023-06-29 10:36:07 -0400

    Added reminder note on Projectors about the commented self-attention

--- chemspace/Projectors.py
+++ chemspace/Projectors.py
@@ -52,6 +52,8 @@ class Projector(nn.Module):
                  **kwargs):
         super(Projector, self).__init__()
 
+        # Deprecated code from when we were using a self-attention layer.
+        # will be reimplemented in the future
         # if kernel_size:
         #     self.conv1 = nn.Conv1d(input_size, hidden_size, kernel_size)
         #     # self.attention = SelfAttention(hidden_size, hidden_size)
