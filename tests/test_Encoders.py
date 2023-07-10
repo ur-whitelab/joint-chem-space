60d5f5d426b52d4f40ee6e2d419d3678126c0749
DataProdeng <96993587+ahmadovra@users.noreply.github.com>
2023-07-10 18:04:06 -0400

    Test class updated for txt

--- tests/test_Encoders.py
+++ tests/test_Encoders.py
@@ -8,7 +8,7 @@ import torch
 
 class TestEncoders:
     @pytest.mark.parametrize('testSMILES',[['CCO'],['CCO','CCO']])
-    def test_encoder_shape(self, testSMILES):
+    def test_sml_encoder_shape(self, testSMILES):
         """
         Test that the encoder returns the correct shape.
         Specifically for ChemBERTa-77M-MLM, the output dimension is 384.
@@ -18,3 +18,14 @@ class TestEncoders:
         batch_size = len(testSMILES)
         test = E.tokenize(testSMILES)
         assert E(test).shape == torch.Tensor(batch_size, 512, 384).shape
+    @pytest.mark.parametrize('testTXT',[['To synthesize CCO, we need to have the following'],['CCOHCOO is synthesized using the following chemicals']])
+    def test_txt_encoder_shape(self, testSMILES):
+        """
+        Test that the encoder returns the correct shape.
+        Specifically for scibert_scivocab_cased, the output dimension is 768.
+
+        """
+        E = cs.Encoder(model_name = "allenai/scibert_scivocab_cased")
+        batch_size = len(testSMILES)
+        test = E.tokenize(testSMILES)
+        assert E(test).shape == torch.Tensor(batch_size, 512, 768).shape
