229e72c5b5afeb46c810f1e75b309b4bb1c412c4
DataProdeng <96993587+ahmadovra@users.noreply.github.com>
2023-07-11 09:32:56 -0400

    testTXT added for txt encoder

--- tests/test_Encoders.py
+++ tests/test_Encoders.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append(".")
 import chemspace as cs
+from transformers import AutoModel
 
 import pytest
 import torch
@@ -18,14 +19,14 @@ class TestEncoders:
         batch_size = len(testSMILES)
         test = E.tokenize(testSMILES)
         assert E(test).shape == torch.Tensor(batch_size, 512, 384).shape
+
     @pytest.mark.parametrize('testTXT',[['To synthesize CCO, we need to have the following'],['CCOHCOO is synthesized using the following chemicals']])
-    def test_txt_encoder_shape(self, testSMILES):
+    def test_txt_encoder_shape(self, testTXT):
         """
         Test that the encoder returns the correct shape.
         Specifically for scibert_scivocab_cased, the output dimension is 768.
-
         """
-        E = cs.Encoder(model_name = "allenai/scibert_scivocab_cased")
-        batch_size = len(testSMILES)
-        test = E.tokenize(testSMILES)
+        E = cs.Encoder(model_name="allenai/scibert_scivocab_cased", model_type=AutoModel)
+        batch_size = len(testTXT)
+        test = E.tokenize(testTXT)
         assert E(test).shape == torch.Tensor(batch_size, 512, 768).shape
