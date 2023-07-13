e77b7edf8a741e5096a92169aed676766084a03c
Mayk Caldas <maykcaldas@gmail.com>
2023-07-13 12:55:38 -0400

    Organizing dependencies (#31)
    * removing openbabel from the mandatory dependencies to make pip work
    
    Since openbabel, rdkit, requests and pytest should only be used during development, they were moved to dev-requirements.txt. The conda environment environment.yml still have all dependencies
    
    * Fixed pyTorch package name. It's called torch in pip
    
    * updated version to 0.0.1
    
    * catched the error if openbabel was not installed by pip
    
    * Imported dataset into chemspace to use DatasetBuilder as a subpackage
    
    * added rdkit as a regular dependency since it's used by DatasetBuilder

--- chemspace/__init__.py
+++ chemspace/__init__.py
@@ -1,4 +1,11 @@
 from .pug_utils import *
-from .obabel_utils import *
+try:
+    from .obabel_utils import *
+except ImportError:
+    print('Openbabel not installed. \n' \
+          'Consider using the conda environment if utilities from OpenBabel will be used.\n\n' \
+          'Install it using: conda env create -f environment.yml')
 from .Encoders import Encoder
 from .Projectors import ProjConfig, Projector
+
+from .Dataset import *
